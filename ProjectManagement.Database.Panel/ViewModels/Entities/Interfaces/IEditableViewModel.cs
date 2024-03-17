a116cbae98b78e9ffcea760000d5cc83e881e297
9Maverick <46315519+9Maverick@users.noreply.github.com>
2024-03-17 15:46:06 +0200

    Add new Interface for view model with edit functionality

---
+++ ProjectManagement.Database.Panel/ViewModels/Entities/Interfaces/IEditableViewModel.cs
@@ -0,0 +1,19 @@
+﻿using ProjectManagement.Database.Domain.Interfaces;
+
+namespace ProjectManagement.Database.Panel.ViewModels.Entities.Interfaces;
+
+public interface IEditableViewModel
+{
+    public bool IsLoaded { get; set; }
+    public bool IsEditing { get; set; }
+    public void Edit();
+    public void Save();
+    public void Cancel();
+    public void Delete();
+}
+
+public interface IEditableChildViewModel : IEditableViewModel
+{
+    public Dictionary<uint?, string> ParentIdNames { get; set; }
+}
+public interface IEditableTeamViewModel : IChildEntityViewModel<ITeam>, IEditableChildViewModel;
