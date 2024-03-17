815a720ffae6c44b65ae8c4fe692c9ee137abac3
9Maverick <46315519+9Maverick@users.noreply.github.com>
2024-03-17 16:37:58 +0200

    Add interfaces for collection view models

---
+++ ProjectManagement.Database.Panel/ViewModels/Collections/Intefaces/IEntityCollectionViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ProjectManagement.Database.Panel.ViewModels.Entities.Interfaces;
+
+namespace ProjectManagement.Database.Panel.ViewModels.Collections.Intefaces;
+
+public interface IEntityCollectionViewModel<T>
+{
+    public List<IEntityViewModel<T>> Entities { get; set; }
+    public T EntityToAdd { get; set; }
+    public void AddEntity();
+}
+
+public interface IChildEntityCollectionViewModel<T>
+{
+    public List<IChildEntityViewModel<T>> Entities { get; set; }
+    public Dictionary<uint?, string> ParentIdNames { get; set; }
+    public T EntityToAdd { get; set; }
+    public void AddEntity();
+}
