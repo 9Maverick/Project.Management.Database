4d87f4eaa01d0af7d2ea3e2fede7175e550321a1
9Maverick <46315519+9Maverick@users.noreply.github.com>
2024-03-17 15:34:29 +0200

    Change services scope

--- ProjectManagement.Database.Panel/Configuration/ServicesConfiguration.cs
+++ ProjectManagement.Database.Panel/Configuration/ServicesConfiguration.cs
@@ -1,13 +1,16 @@
 ﻿using ProjectManagement.Database.Panel.ViewModels.Collections;
 using ProjectManagement.Database.Panel.ViewModels.Collections.Intefaces;
+using ProjectManagement.Database.Panel.ViewModels.Entities.Interfaces;
+using ProjectManagement.Database.Panel.ViewModels.Entities.Page;
 
 namespace ProjectManagement.Database.Panel.Configuration;
 public static class ServicesConfiguration
 {
     public static void AddViewModels(this IServiceCollection services)
     {
-        services.AddScoped<IProjectsCollectionViewModel, ProjectsCollectionViewModel>();
-        services.AddScoped<IUsersCollectionViewModel, UsersCollectionViewModel>();
-        services.AddScoped<ITeamsCollectionViewModel, TeamsCollectionViewModel>();
+        services.AddTransient<IProjectsCollectionViewModel, ProjectsCollectionViewModel>();
+        services.AddTransient<IUsersCollectionViewModel, UsersCollectionViewModel>();
+        services.AddTransient<ITeamsCollectionViewModel, TeamsCollectionViewModel>();
+        services.AddTransient<IEditableTeamViewModel, TeamPageViewModel>();
     }
 }
