6ff1ff765d2de393f781eeb45dc1a94468081567
9Maverick <46315519+9Maverick@users.noreply.github.com>
2024-03-20 14:43:46 +0200

    Move variables to more convinient place

--- ProjectManagement.Database.Domain/Entities/Comment.cs
+++ ProjectManagement.Database.Domain/Entities/Comment.cs
@@ -6,10 +6,13 @@ public class Comment : IComment
 {
     public uint Id { get; set; }
     public uint UserId { get; set; }
-    public User? User { get; set; }
     public uint TicketId { get; set; }
-    public Ticket? Ticket { get; set; }
+
     public string Text { get; set; }
+
+    public User? User { get; set; }
+    public Ticket? Ticket { get; set; }
+
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
     public Comment() { }
