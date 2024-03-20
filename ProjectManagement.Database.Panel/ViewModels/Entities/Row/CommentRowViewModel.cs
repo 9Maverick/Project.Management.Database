36f431e8e32897778d263bb3da985cb6e07feffc
9Maverick <46315519+9Maverick@users.noreply.github.com>
2024-03-20 14:59:30 +0200

    Add comment row controller

--- ProjectManagement.Database.Panel/ViewModels/Entities/Row/CommentRowViewModel.cs
+++ ProjectManagement.Database.Panel/ViewModels/Entities/Row/CommentRowViewModel.cs
@@ -1,20 +1,21 @@
 ﻿using ProjectManagement.Database.Data;
 using ProjectManagement.Database.Domain.Entities;
 using ProjectManagement.Database.Domain.Interfaces;
+using ProjectManagement.Database.Domain.Models;
 using ProjectManagement.Database.Panel.ViewModels.Entities.Interfaces;
 
 namespace ProjectManagement.Database.Panel.ViewModels.Entities.Row;
 
-public class CommentRowViewModel : IOwnedEntityViewModel<IComment>
+public class CommentRowViewModel : IOwnedEntityPageViewModel<IComment>
 {
 
     private DatabaseContext _context;
-    private Comment _team;
+    private Comment _comment;
 
     public uint Id { get; set; }
     public IComment Entity
     {
-        get => _team;
+        get => _comment;
         set
         {
             return;
@@ -25,15 +26,72 @@ public class CommentRowViewModel : IOwnedEntityViewModel<IComment>
 
     public IUser Owner { get; private set; }
 
-    public CommentRowViewModel(Comment team, DatabaseContext context)
+    public Dictionary<uint, string> OwnerSource { get; private set; }
+
+    public bool IsLoaded { get; set; }
+    public bool IsEditing { get; set; }
+
+    public CommentRowViewModel(Comment comment, DatabaseContext context)
     {
         _context = context;
-        _team = team;
+        _comment = comment;
 
-        Id = _team.Id;
+        Id = _comment.Id;
 
         LoadOwner();
     }
+
+    #region Controls
+
+    public void Cancel()
+    {
+        Entity = _comment;
+        IsEditing = false;
+    }
+
+    public void Delete()
+    {
+        _context.Comments.Remove(_comment);
+        _context.SaveChanges();
+
+        IsLoaded = false;
+    }
+
+    public void Edit()
+    {
+        Entity = new CommentModel(_comment);
+        IsEditing = true;
+    }
+
+    public void Save()
+    {
+        _comment.SetComment(Entity);
+        _context.SaveChanges();
+
+        IsEditing = false;
+
+        LoadComment();
+    }
+
+    #endregion
+
+    private void LoadComment()
+    {
+        if (Id == 0) return;
+
+        var comment = _context.Comments.Find(Id);
+
+        if (comment == null) return;
+
+        _comment = comment;
+        Entity = _comment;
+
+        LoadOwner();
+        LoadOwnerVariants();
+
+        IsLoaded = true;
+    }
+
     private void LoadOwner()
     {
         var ownerId = Entity.UserId;
@@ -42,5 +100,17 @@ public class CommentRowViewModel : IOwnedEntityViewModel<IComment>
         Owner = _context.Users
             .Where(user => user.Id == ownerId)
             .FirstOrDefault();
+    }
+    private void LoadOwnerVariants()
+    {
+        OwnerSource = new Dictionary<uint, string>();
+
+        var userList = _context.Users
+            .ToList();
+
+        foreach (var user in userList)
+        {
+            OwnerSource.Add(user.Id, user.Name);
+        }
     }
 }
